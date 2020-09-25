17a3bc994f9473326825db2830f774a12c918978
pratikshinde97 <pratikshinde972@gmail.com>
2020-09-25 11:21:11 +0530

    change in image size and addedappbar size in constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kBottomTextSize = TextStyle(fontSize: 14,color: Colors.black);
+const kBottomTextSize = TextStyle(fontSize: 14, color: Colors.black);
 const kTextSize16 = TextStyle(fontSize: 16, color: Colors.black);
 const kTextSize14 = TextStyle(fontSize: 14, color: Colors.black);
+const kAppTextSize18 = TextStyle(fontSize: 18, color: Colors.black);
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,62 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:market_place/constants.dart';
+import 'package:market_place/model/category_list.dart';
 import 'package:market_place/screens/category.dart';
-import 'file:///D:/Auth/android-jwt-authentication-master/market_place/lib/model/category_list.dart';
 import 'package:market_place/utilities/carousol_class.dart';
 
 class HomePage extends StatefulWidget {
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
-
-
-  List<CategoryList> categoryList =[
-    CategoryList(categoryName: 'cat-1',imageCategory:  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',),
-    CategoryList(categoryName: 'cat-2',imageCategory:  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',),
-    CategoryList(categoryName: 'cat-3',imageCategory:  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',),
-    CategoryList(categoryName: 'cat-4',imageCategory:  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',),
-
+  List<CategoryList> categoryList = [
+    CategoryList(
+      categoryName: 'cat-1',
+      imageCategory:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    ),
+    CategoryList(
+      categoryName: 'cat-2',
+      imageCategory:
+          'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    ),
+    CategoryList(
+      categoryName: 'cat-3',
+      imageCategory:
+          'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    ),
+    CategoryList(
+      categoryName: 'cat-4',
+      imageCategory:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    ),
   ];
 
-
-List<Widget> categoryContainer() {
-  List<Container> newContainer =[];
-  for(int i=0;i<categoryList.length;i++){
-     newContainer.add(Container(
-       decoration:BoxDecoration(
-         color: Colors.white,
-         borderRadius: BorderRadius.circular(10),
-       ),
-       padding: const EdgeInsets.all(8),
-       child: GestureDetector(
-         onTap: (){
-           Navigator.push(context, MaterialPageRoute(builder: (context)=>Category()));
-         },
-         child: Column (
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: <Widget>[
-             //Icon(categoryList[i].iconCategory,size: 40,),
-             Image.network(categoryList[i].imageCategory, height: 70, width: 90),
-             SizedBox(height: 12,),
-             Text(categoryList[i].categoryName,style: kBottomTextSize,)
-           ],
-         ),
-       ),
-     ));
+  List<Widget> categoryContainer() {
+    List<Container> newContainer = [];
+    for (int i = 0; i < categoryList.length; i++) {
+      newContainer.add(Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.all(8),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Category()));
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              //Icon(categoryList[i].iconCategory,size: 40,),
+              Expanded(
+                flex: 3,
+                child: Image.network(categoryList[i].imageCategory,
+                    height: 70, width: 90),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Expanded(
+                flex: 1,
+                child: Text(
+                  categoryList[i].categoryName,
+                  style: kBottomTextSize,
+                ),
+              )
+            ],
+          ),
+        ),
+      ));
+    }
+    return newContainer;
   }
-  return newContainer;
-}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEDF0EE),
       appBar: AppBar(
-        title: Text('Market Place'),
+        title: Text(
+          'Market Place',
+          style: kAppTextSize18,
+        ),
         backgroundColor: Colors.indigo,
       ),
       body: SafeArea(
@@ -64,9 +90,10 @@ List<Widget> categoryContainer() {
           height: MediaQuery.of(context).size.height,
           child: ListView(
             children: <Widget>[
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               CarouselClass(),
-
               Container(
                 child: CustomScrollView(
                   shrinkWrap: true,
@@ -79,7 +106,6 @@ List<Widget> categoryContainer() {
                         mainAxisSpacing: 26,
                         crossAxisCount: 2,
                         children: categoryContainer(),
-
                       ),
                     ),
                   ],
@@ -88,7 +114,6 @@ List<Widget> categoryContainer() {
             ],
           ),
         ),
-
       ),
     );
   }
