c4d4ffe3247f3493116ba61089a6c6fe35dc114a
sat-tish <satish30yash@gmail.com>
2020-10-12 20:07:44 +0530

    changes

--- lib/screens/product_description.dart
+++ lib/screens/product_description.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:market_place/constants.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:market_place/utilities/caurousol_product_description.dart';
+import 'package:market_place/utilities/product_image_slider.dart';
+
 class ProductDescription extends StatefulWidget {
   final String productName;
   ProductDescription({this.productName});
@@ -18,34 +22,27 @@ class _ProductDescriptionState extends State<ProductDescription> {
       body: SafeArea(
         child: ListView(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(width: 16,),
-                Text(productName,style: TextStyle(fontSize: 16),),
-              ],
-            ),
-            Divider(),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 16),
-              child: Container(
-                height: 150,
-                color: Colors.indigoAccent,
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(productName,style: TextStyle(fontSize: 16),),
+                ],
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Text('This is our new Product, it is better & best of its category. '
-                      'This is our new Product, it is better & best of its category.'
-                      'This is our new Product, it is better & best of its category.'
-                      'This is our new Product, it is better & best of its category.'
-                      'This is our new Product, it is better & best of its category.'
-                      'This is our new Product, it is better & best of its category.',style: TextStyle(fontSize: 16),),
-                ),
-              ],
+            Divider(),
+            Container(
+              height: 200,
+              child: ProductImageSlider()),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text('This is our new Product, it is better & best of its category. '
+                  'This is our new Product, it is better & best of its category.'
+                  'This is our new Product, it is better & best of its category.'
+                  'This is our new Product, it is better & best of its category.'
+                  'This is our new Product, it is better & best of its category.'
+                  'This is our new Product, it is better & best of its category.',style: TextStyle(fontSize: 16),),
             ),
             SizedBox(height: 16,),
           ],
---
+++ lib/utilities/product_image_slider.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+
+import 'package:market_place/model/slider_model.dart';
+import 'package:market_place/screens/home_page.dart';
+
+class ProductImageSlider extends StatefulWidget {
+  @override
+  _ProductImageSliderState createState() => _ProductImageSliderState();
+}
+
+class _ProductImageSliderState extends State<ProductImageSlider> {
+  List<SliderModel> mySLides = new List<SliderModel>();
+  int slideIndex = 0;
+  //PageController controller;
+
+  Widget _buildPageIndicator(bool isCurrentPage) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 2.0),
+      height: isCurrentPage ? 10.0 : 6.0,
+      width: isCurrentPage ? 10.0 : 6.0,
+      decoration: BoxDecoration(
+        color: isCurrentPage ? Color(0xFF344955) : Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    mySLides = getSlides();
+    //controller = new PageController();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          Container(
+            height: 150,
+            width: 300,
+            child: PageView(
+              //controller: controller,
+              onPageChanged: (index) {
+                setState(() {
+                  slideIndex = index;
+                });
+              },
+              children: <Widget>[
+                SlideTile(
+                  imagePath: mySLides[0].getImageAssetPath(),
+                ),
+                SlideTile(
+                  imagePath: mySLides[1].getImageAssetPath(),
+                ),
+                SlideTile(
+                  imagePath: mySLides[2].getImageAssetPath(),
+                ),
+                SlideTile(
+                  imagePath: mySLides[3].getImageAssetPath(),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height:50,
+                child: Row(
+                  children: [
+                    for (int i = 0; i < 4; i++)
+                      i == slideIndex
+                          ? _buildPageIndicator(true)
+                          : _buildPageIndicator(false),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      );
+  }
+}
+
+class SlideTile extends StatelessWidget {
+  final String imagePath, title, desc;
+
+  SlideTile({this.imagePath, this.title, this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Image.asset(imagePath,fit: BoxFit.cover,),
+    );
+  }
+}
+
