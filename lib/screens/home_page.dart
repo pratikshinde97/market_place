3377a01bf917b77711b120a2cdd4749ba3ee8149
pratikshinde97 <pratikshinde972@gmail.com>
2020-10-13 12:47:56 +0530

    added fonts and other changes

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:market_place/constants.dart';
 import 'package:market_place/model/category_list.dart';
@@ -15,6 +16,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   CategoryList cat = CategoryList();
   String categoryName;
+  Future<bool> _onWillPop() {
+    return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Are you sure'),
+            content: Text('Do you want to exit ?'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text('No'),
+              ),
+              FlatButton(
+                onPressed: () => exit(0),
+                /*Navigator.of(context).pop(true)*/
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
 
   List<Widget> categoryContainer() {
     List<Container> newContainer = [];
@@ -64,119 +86,121 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //backgroundColor: Color(0xFFEDF0EE),
-      backgroundColor: Color(0xFFE8EAF6),
-      appBar: AppBar(
-        title: Text(
-          'Market Place',
-          //style: kAppBarTextSize18,
-          style: TextStyle(fontSize: 18, color: Colors.white),
-        ),
-        actions: <Widget>[
-          InkWell(
-            child: Icon(Icons.shopping_cart),
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Cart()));
-            },
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        //backgroundColor: Color(0xFFEDF0EE),
+        backgroundColor: Color(0xFFE8EAF6),
+        appBar: AppBar(
+          title: Text(
+            'Market Place',
+            //style: kAppBarTextSize18,
+            style: TextStyle(fontSize: 18, color: Colors.white),
           ),
-          SizedBox(
-            width: 10,
-          )
-        ],
-        backgroundColor: Color(0xFF344955),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: ListView(
-                  children: <Widget>[
+          actions: <Widget>[
+            InkWell(
+              child: Icon(Icons.shopping_cart),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Cart()));
+              },
+            ),
+            SizedBox(
+              width: 10,
+            )
+          ],
+          backgroundColor: Color(0xFF344955),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: ListView(
+                    children: <Widget>[
 //                    SizedBox(
 //                      height: 10,
 //                    ),
-                    Container(height: 140, child: CarouselClass()),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                            child: Divider(
-                          color: Color(0xFF344955),
-                          indent: 20,
-                        )),
-                        Expanded(
-                            child: Center(
-                                child: Text('Categories',
-                                    style: TextStyle(
-                                        fontSize: 16,
-                                        color: Colors.black87,
-                                        fontWeight: FontWeight.bold)))),
-                        Expanded(
-                            child: Divider(
-                          color: Color(0xFF344955),
-                          endIndent: 20,
-                        )),
-                      ],
-                    ),
-                    //Center(child: Text('Categories',style: TextStyle(fontSize: 16,color: Colors.black),)),
-
-                    Container(
-                      child: CustomScrollView(
-                        shrinkWrap: true,
-                        primary: false,
-                        slivers: <Widget>[
-                          SliverPadding(
-                            padding: const EdgeInsets.all(16),
-                            sliver: SliverGrid.count(
-                              crossAxisSpacing: 16,
-                              mainAxisSpacing: 16,
-                              crossAxisCount: 2,
-                              children: categoryContainer(),
-                            ),
-                          ),
+                      Container(height: 140, child: CarouselClass()),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                              child: Divider(
+                            color: Color(0xFF344955),
+                            indent: 20,
+                          )),
+                          Expanded(
+                              child: Center(
+                                  child: Text('Categories',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.black87,
+                                          fontWeight: FontWeight.bold)))),
+                          Expanded(
+                              child: Divider(
+                            color: Color(0xFF344955),
+                            endIndent: 20,
+                          )),
                         ],
                       ),
-                    )
-                  ],
+                      //Center(child: Text('Categories',style: TextStyle(fontSize: 16,color: Colors.black),)),
+
+                      Container(
+                        child: CustomScrollView(
+                          shrinkWrap: true,
+                          primary: false,
+                          slivers: <Widget>[
+                            SliverPadding(
+                              padding: const EdgeInsets.all(16),
+                              sliver: SliverGrid.count(
+                                crossAxisSpacing: 16,
+                                mainAxisSpacing: 16,
+                                crossAxisCount: 2,
+                                children: categoryContainer(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Container(
-              height: 54,
-              color: Color(0xFF344955),
-              child: Row(
-                children: <Widget>[
-                  NewExpanded(
-                    iconData: Icons.home,
-                    tabName: 'Home',
-                    color: Colors.yellow[700],
-                  ),
-                  NewExpanded(
-                    onTap: () {
-                      categoryName = 'All Categories';
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Category(categoryName)));
-                    },
-                    color: Colors.white,
-                    iconData: Icons.category,
-                    tabName: 'Category',
-                  ),
-                  NewExpanded(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Orders()));
-                    },
-                    iconData: Icons.business,
-                    color: Colors.white,
-                    tabName: 'Orders',
-                  ),
+              Container(
+                height: 54,
+                color: Color(0xFF344955),
+                child: Row(
+                  children: <Widget>[
+                    NewExpanded(
+                      iconData: Icons.home,
+                      tabName: 'Home',
+                      color: Colors.yellow[700],
+                    ),
+                    NewExpanded(
+                      onTap: () {
+                        categoryName = 'All Categories';
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Category(categoryName)));
+                      },
+                      color: Colors.white,
+                      iconData: Icons.category,
+                      tabName: 'Category',
+                    ),
+                    NewExpanded(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Orders()));
+                      },
+                      iconData: Icons.business,
+                      color: Colors.white,
+                      tabName: 'Orders',
+                    ),
 //                  NewExpanded(
 //                    onTap: () {
 //                      Navigator.push(context,
@@ -185,10 +209,11 @@ class _HomePageState extends State<HomePage> {
 //                    iconData: Icons.shopping_cart,
 //                    tabName: 'Cart',
 //                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
